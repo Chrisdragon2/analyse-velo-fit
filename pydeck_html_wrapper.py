a0a24360b04e4de5e94e4f61eafbc641da361ca0
Chrisdragon2 <chris.dspires@gmail.com>
2025-11-12 00:48:04 +0100

    Update pydeck_html_wrapper.py

--- pydeck_html_wrapper.py
+++ pydeck_html_wrapper.py
@@ -46,6 +46,7 @@ def generate_deck_html(pydeck_object: pdk.Deck, mapbox_key: str) -> str:
 
     <script type="text/javascript">
         // Définir la clé Mapbox (sécurisée par restriction d'URL)
+        // C'est redondant mais ne fait pas de mal.
         mapboxgl.accessToken = "{mapbox_key}";
 
         // 1. Recevoir la spécification JSON injectée depuis Python
@@ -63,7 +64,12 @@ def generate_deck_html(pydeck_object: pdk.Deck, mapbox_key: str) -> str:
             controller: true,
             
             // 5. Spécifier le conteneur
-            container: 'deck-gl-map'
+            container: 'deck-gl-map',
+
+            // 6. --- LA CORRECTION DU BUG "ÉCRAN NOIR" ---
+            // Le constructeur JS 'new Deck()' a besoin de la clé ici,
+            // pas seulement 'mapboxgl.accessToken'
+            mapboxApiAccessToken: "{mapbox_key}" 
         }});
 
     </script>
